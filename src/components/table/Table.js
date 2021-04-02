00da688f96d95e065082b413f71038ace8732d81
iamdruce <iamdruce@gmail.com>
2021-04-02 19:33:08 +0300

    Finnish base framework functional

---
+++ src/components/table/Table.js
@@ -0,0 +1,10 @@
+import {createTable} from '@/components/table/table.template'
+import {ExcelComponent} from '@core/ExcelComponent'
+
+export class Table extends ExcelComponent {
+  static className = 'excel__table'
+
+  toHTML() {
+    return createTable(25)
+  }
+}
